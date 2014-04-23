3c935a7e33a89c1172c9400f828c2a5c5bb024e5
marvinick <marviluw@gmail.com>
2014-04-23 08:40:14 -0500

    dev den

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Seasalt::Application.config.secret_key_base = '1378749b6bae7db547dc2b1d29481d307a299fdc52ad5f652914dc75becd24256f3a53f6fc50fe44282c56de29fe6973a49adeaee0e5bed707d4340b8cf2658b'
