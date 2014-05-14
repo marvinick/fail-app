805d42b57286dfc2173f81cb59a1012372f5e86b
marvinick <marviluw@gmail.com>
2014-05-14 08:34:46 -0500

    more feature tests on users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @users = User.all
+  end
+
+
   private
 
   def user_params
--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -3,5 +3,9 @@ require 'spec_helper'
 feature "User signup" do
   scenario "User sign up with valid info" do
     visit users_path
+    fill_in "Name", with: "Marvin"
+    fill_in "Email", with: "daddy@daddy.com"
+    click_button "Register"
+    expect(page).to have_content @users
   end
 end
