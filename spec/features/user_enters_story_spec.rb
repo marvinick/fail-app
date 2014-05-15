9aba4f26dd39c2566fac4fa27d07c34f3e2ea332
marvinick <marviluw@gmail.com>
2014-05-15 15:31:51 -0500

    rspec user enters story

--- spec/features/user_enters_story_spec.rb
+++ spec/features/user_enters_story_spec.rb
@@ -3,5 +3,8 @@ require 'spec_helper'
 feature 'user enters story' do
   scenario 'user enters the right input for stories' do
     visit stories_path
+    fill_in "Story", with: "Text"
+    click_button "Tell your Story"
+    expect(page).to have_content @stories
   end
 end
