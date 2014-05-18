f17ce193c389d2d555ee724b55fbe74d19649af9
marvinick <marviluw@gmail.com>
2014-05-18 15:33:38 -0500

    ui

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -5,6 +5,11 @@ feature "User signup" do
     visit users_path
     fill_in "Name", with: "Marvin"
     fill_in "Email", with: "daddy@daddy.com"
+    fill_in "Job Title", with: "chef"
+    fill_in "Birthday", with: "12/12/1991"
+    fill_in "Years of experience", with: "19 years"
+    fill_in "Education", with: "High School"
+    fill_in "Story", with: "I want to become a sushi chef"
     click_button "Register"
     expect(page).to have_content @users
   end
